7d13e9ac051b735d69c9f23f5616ef59f874474e
Corey Trombley <corey.trombley33@gmail.com>
2013-06-21 22:39:10 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CuppleHub::Application.config.secret_token = '888c7a017e585d79bc85bf60d210a951e050f95052bda93491c1ebc6bcfc2ed748cb4cbbdfa8db3f2b156894ba5655724aff76b16f14b3bcc7b0d19e67fe5efc'

60d96b9fdf853352723a1bc9ba31cea5de049b5e
Corey Trombley <corey.trombley33@gmail.com>
2013-06-22 11:08:12 -0400

    added login form

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def resource_name
+    :couple
+  end
+ 
+  def resource
+     @resource  ||= Couple.new
+  end
+ 
+  def devise_mapping
+     @devise_mapping  ||= Devise.mappings[:couple]
+  end
 end

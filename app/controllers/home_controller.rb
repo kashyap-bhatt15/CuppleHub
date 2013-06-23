5ebf46b4e0c0c7f50a3d7f56bcd0b98e39490e0b
Kashyap Bhatt <kashyap.bhatt15@gmail.com>
2013-06-23 11:02:00 -0400

    up until profile form is filled up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,14 @@ class HomeController < ApplicationController
   def index
   end
   def show
-    @couple = Couple.find(params[:id])
+    #@couple = Couple.find(params[:id])
   end
   def modal1
   end
+  
   def splash
   end
+  
   def profile
   end
 end

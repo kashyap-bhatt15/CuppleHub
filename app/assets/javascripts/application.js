77052a30892c0542afe401493e4dd59a1ecd4bfd
Corey Trombley <corey.trombley33@gmail.com>
2013-06-23 02:14:38 -0400

    fixed matching page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,6 @@ jQuery(document).ready(function($) {
   $('#centerbutton').click(function(e) {
     e.preventDefault();
     $('.circles li a').toggleClass('out');
-    $(this).toggleClass('centerbuttonon');
-  });;
+    // $(this).toggleClass('centerbuttonon');
+  });
 });
